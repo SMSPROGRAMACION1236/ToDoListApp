6afe5fb94849a6e4cf3f8481374b28282a1216e7
wdeakdlnaed <76114338+wdeakdlnaed@users.noreply.github.com>
2025-06-17 21:30:07 +0000

    Update task_manager.py
    Added a task counter

--- src/functions/task_manager.py
+++ src/functions/task_manager.py
@@ -1,16 +1,20 @@
 import tkinter as tk
 
-
 class TaskManager:
-  def __init__(self):
-    self.tasks = []
-  def add_task(self, task):
-    if task:
-      self.tasks.append(task)
-      return True
-    return False
-  def remove_task(self, task):
-      if task:
-        self.tasks.remove(task)
-        return True
-      return False
+    def __init__(self):
+        self.tasks = []
+
+    def add_task(self, task):
+        if task:
+            self.tasks.append(task)
+            return True
+        return False
+
+    def remove_task(self, task):
+        if task in self.tasks:
+            self.tasks.remove(task)
+            return True
+        return False
+
+    def get_all_tasks(self):
+        return self.tasks
